b1c822c980de38d2b38efa8efdd7b42cf3462d69
BTD <169353918+btd-git@users.noreply.github.com>
2024-05-19 00:28:27 +0530

    Update 2-configuration.sh

--- 2-configuration.sh
+++ 2-configuration.sh
@@ -95,7 +95,7 @@ systemctl enable acpid
 # ------------------------------------------------------
 # Grub installation
 # ------------------------------------------------------
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --removable
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # ------------------------------------------------------
